4aff86092a0ee4bbb74ebb97d2224874f377e863
agladyshev <agladyshev@inat.ai>
2020-10-19 20:15:44 +0300

    add function for drop stop words

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,10 +1,12 @@
 import re 
 
 from nltk.tokenize import WordPunctTokenizer
+from nltk.corpus import stopwords
 from bs4 import BeautifulSoup
 
 
 __word_punct_tokenizer = WordPunctTokenizer()
+__stop_words = stopwords.words('english')
 
 
 def remove_html_tags(html):
@@ -18,3 +20,7 @@ def stay_only_a_z(text):
 
 def tokenize_by_word(text):
     return ' '.join(__word_punct_tokenizer.tokenize(text))
+
+
+def remove_stop_words(text):
+    return ' '.join([word for word in text.split() if word.lower() not in __stop_words])
